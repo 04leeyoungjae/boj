6694ea88b53a88c92d2d6573be8347e46ea87c12
karinysis <127736867+karinysis@users.noreply.github.com>
2023-08-04 18:33:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 144 ms, Memory: 116156 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,14 @@
+def sol(n):
+    if n in d:
+        return d[n]
+    else:
+        d[n]=[sol(n-1)[0]+sol(n-2)[0],sol(n-1)[1]+sol(n-2)[1]]
+        return d[n]
+
+
+for i in range(int(input())):
+    d={0:[1,0], 1:[0,1]}
+    cnt=[0,0]
+    n=int(input())
+    sol(n)
+    print(d[n][0],d[n][1])
